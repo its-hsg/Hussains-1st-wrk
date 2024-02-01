602bd136d253e8b519b459a83f7d3fa3f0aa59b0
Hussain Salis <156366620+its-hsg@users.noreply.github.com>
2024-02-01 21:47:17 +0100

    Add files via upload

---
+++ Personal PRJ/HTML&CSS/3. interactive photo gallary/iph.js
@@ -0,0 +1,18 @@
+// Inside the script tag in your HTML file
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+
+  gallery.addEventListener('click', function (event) {
+    if (event.target.tagName === 'IMG') {
+      const imgSrc = event.target.src;
+      modalImage.src = imgSrc;
+      modal.style.display = 'block';
+    }
+  });
+
+  modal.addEventListener('click', function () {
+    modal.style.display = 'none';
+  });
+});
