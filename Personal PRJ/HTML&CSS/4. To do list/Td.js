329f229980221a66b4edbfec4544de64a711d683
Hussain Salis <156366620+its-hsg@users.noreply.github.com>
2024-02-20 21:11:11 +0100

    Update Td.js

--- Personal PRJ/HTML&CSS/4. To do list/Td.js
+++ Personal PRJ/HTML&CSS/4. To do list/Td.js
@@ -10,7 +10,7 @@ function addTask(event) {
   if (taskText !== '') {
     const taskItem = document.createElement('li'); // Create new task item
     taskItem.textContent = taskText; // Set task text
-    taskList.appendChild(taskItem); // Append task item to task list
+    taskList.appendChild(taskItem);// Append task item to task list
     taskInput.value = '';//Clear input field
   }
 }
