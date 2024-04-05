e9782921b92f2750d6751e10306dbe1f393e7d55
Hussain Salis <156366620+its-hsg@users.noreply.github.com>
2024-04-05 23:15:04 +0100

    Update quiz.js

--- Personal PRJ/JavaScript/quiz.js
+++ Personal PRJ/JavaScript/quiz.js
@@ -1,7 +1,7 @@
  function checkAnswers() {
   let score = 0;
   
-  const q1Answer = document.querySelector('input[name="q1"]:checked').value;
+  const q1Answer = document.querySelector ('input[name="q1"]:checked').value;
   const q2Answer = document.querySelector('input[name="q2"]:checked').value;
   const q3Answer = document.querySelector('input[name="q3"]:checked').value;
   
