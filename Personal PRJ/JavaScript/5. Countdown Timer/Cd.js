f2771996c947e7a810ec10ad276998c293b609d7
Hussain Salis <156366620+its-hsg@users.noreply.github.com>
2024-05-29 17:01:52 +0100

    Update Cd.js

--- Personal PRJ/JavaScript/5. Countdown Timer/Cd.js
+++ Personal PRJ/JavaScript/5. Countdown Timer/Cd.js
@@ -1,5 +1,5 @@
 // Set the countdown time in seconds
-let countdownTime = 1000; // for 
+let countdownTime = 1000; /
 
 const countdownElement = document.getElementById('countdown');
 const minutesElement = document.getElementById('minutes');
